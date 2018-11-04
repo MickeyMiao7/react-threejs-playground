1accb9189e16c69cf95e7c4cbd21c40f91ec6790
Mickey <327191467@qq.com>
2018-11-04 23:08:47 +0800

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,49 @@ import vertexShader from './vertexShader.glsl';
 import { Route, Switch } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 import Introduction from './Introduction';
+import { Menu, Icon } from 'antd'; 
+const SubMenu = Menu.SubMenu;
 
 class App extends Component {
+  rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+
+  state = {
+    openKeys: ['sub1'],
+  };
+
+  onOpenChange = (openKeys) => {
+    const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
+    if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      this.setState({ openKeys });
+    } else {
+      this.setState({
+        openKeys: latestOpenKey ? [latestOpenKey] : [],
+      });
+    }
+  }
   render() {
     return (
       <HashRouter>
         <div id="three-playground">
           <aside>
-            <ul>
-              <li>Basic 1</li>
-              <li> 2</li>
-              <li>3 </li>
-            </ul>
+            <Menu mode="inline" openKeys={this.state.openKeys} onOpenChange={this.onOpenChange}>
+              <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                <Menu.Item key="1">Option 1</Menu.Item>
+                <Menu.Item key="2">Option 2</Menu.Item>
+                <Menu.Item key="3">Option 3</Menu.Item>
+                <Menu.Item key="4">Option 4</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
+            </Menu>
           </aside>
           <main>
             <Switch>
